0b985c063a6b693c8e5b585b60877d83f820e274
MMMartt <nielumartin@hotmail.com>
2018-09-17 19:27:16 +0800

    add chalk and inquirer

--- configurations/index.js
+++ configurations/index.js
@@ -1,6 +1,12 @@
 module.exports = {
-  urls: [
-    'https://github.com/MMMartt/hust-courses-to-ics.git',
-    'https://github.com/gript/public-script-demo.git'
+  scripts: [
+    {
+      type: 'git',
+      url: 'https://github.com/MMMartt/hust-courses-to-ics.git',
+    },
+    {
+      type: 'git',
+      url: 'https://github.com/gript/public-script-demo.git',
+    }
   ]
 }
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const execa = require('execa')
-const scripts = require('./configurations').urls
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const { scripts } = require('./configurations')
+// TODO: start script -> load configurations -> load scripts -> check updates -> ask if update -> update or exit -> updated and push inform
 
+console.log(chalk.blue('Loading configurations...'))
 ;(async () => {
-  const stdout = await Promise.all(Array.from(scripts).map(url => execa.shell(`git clone --depth 1 --single-branch ${url}`)))
-  console.log(stdout)
+  const { shouldUpdate } = await inquirer.prompt([{
+    name: 'shouldUpdate',
+    type: 'confirm',
+    message: `\n` + scripts.map(({ type, url }) => `${chalk.blue(type)}: ${chalk.grey(url)}\n`) + `\n` +
+    `Update this dependency?`
+  }])
 })()
+// ;(async () => {
+//   const stdout = await Promise.all(Array.from(scripts).map(url => execa.shell(`git clone --depth 1 --single-branch ${url}`)))
+//   console.log(stdout)
+// })()
