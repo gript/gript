a34d0cc423de2b273cd81df6b690de7553467c58
MMMartt <nielumartin@hotmail.com>
2018-09-18 15:29:24 +0800

    should support git configuration(with bug exist)

--- configurations/index.js
+++ configurations/index.js
@@ -2,10 +2,12 @@ module.exports = {
   scripts: [
     {
       type: 'git',
+      name: 'hust-courses-to-ics',
       url: 'https://github.com/MMMartt/hust-courses-to-ics.git',
     },
     {
       type: 'git',
+      name: 'gript-demo',
       url: 'https://github.com/gript/public-script-demo.git',
     }
   ]
--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 const execa = require('execa')
+const getStream = require('get-stream')
 const inquirer = require('inquirer')
 const chalk = require('chalk')
+const globby = require('globby')
 const { scripts } = require('./configurations')
 // TODO: start script -> load configurations -> load scripts -> check updates -> ask if update -> update or exit -> updated and push inform
 
-console.log(chalk.blue('Loading configurations...'))
-;(async () => {
+let scriptsCache = []
+
+const getExistScriptDirectories = async () => {
+  scriptsCache = await globby('scripts/*/', ['!README*'])
+  return scriptsCache
+}
+
+const existScipt = async name => {
+  if (!scriptsCache.length) {
+    const locals = await getExistScriptDirectories()
+    return locals.includes(name)
+  }
+  return scriptsCache.includes(name)
+}
+
+const generateSingle = async ({ type, url, name }) =>
+  chalk.blue(type) + ': ' + chalk.grey(url) + chalk.blue(' LOCAL:') + (await existScipt(name) ? chalk.green('Y') : chalk.red('N')) + '\n'
+
+const generateLoadingMsg = async ([curr, ...rest]) => rest.length
+  ? `${await generateSingle(curr)}${await generateLoadingMsg(rest)}`
+  : `${await generateSingle(curr)}`
+
+const gitCloneRepo = async ([{ url, name }, ...others]) => {
+  const stream = execa.shell(`git clone --depth 1 --single-branch ${url} ${name}`, { cwd: 'scripts' }).stdout
+  console.log(await getStream(stream))
+  if (others.length) await gitCloneRepo(others)
+}
+
+const main = async () => {
   const { shouldUpdate } = await inquirer.prompt([{
     name: 'shouldUpdate',
     type: 'confirm',
-    message: `\n` + scripts.map(({ type, url }) => `${chalk.blue(type)}: ${chalk.grey(url)}\n`) + `\n` +
-    `Update this dependency?`
+    message: chalk.blue('Loading configurations & local scripts...\n') + await generateLoadingMsg(scripts) + `Update scripts?`
   }])
-})()
-// ;(async () => {
-//   const stdout = await Promise.all(Array.from(scripts).map(url => execa.shell(`git clone --depth 1 --single-branch ${url}`)))
-//   console.log(stdout)
-// })()
+
+  if (shouldUpdate) {
+    await gitCloneRepo(scripts)
+  }
+}
+
+main().then(ret => {
+  console.log(ret)
+})
+
